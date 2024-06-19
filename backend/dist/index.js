0a8c4b939fe9a9857726595e8e62b8b1d90e9abe
nitverse <nmakwana496@gmail.com>
2024-06-19 22:34:55 +0530

    Created http server to verify the status of websocket

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,16 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const http_1 = __importDefault(require("http"));
-const server = http_1.default.createServer();
+const server = http_1.default.createServer((req, res) => {
+    if (req.url === "/status" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("WebSocket server is running\n");
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found\n");
+    }
+});
 const wss = new ws_1.WebSocketServer({ server });
 let senderSocket = null;
 let recieverSocket = null;
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-    ws.on('message', function message(data) {
+wss.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+    ws.on("message", function message(data) {
         const message = JSON.parse(data);
         console.log(message);
     });
-    ws.send('something');
+    ws.send("something");
 });
-server.listen(6969);
+const PORT = 6969;
+server.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,46 @@
 import { WebSocketServer } from "ws";
-import http from 'http'
+import http from "http";
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url === "/status" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("WebSocket server is running\n");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found\n");
+  }
+});
+
+
 const wss = new WebSocketServer({ server });
 
 let senderSocket: null | WebSocket = null;
 let recieverSocket: null | WebSocket = null;
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
 
+<<<<<<< HEAD
     ws.on('message', function message(data: any) {
         const message = JSON.parse(data);
      //   console.log(message);
         
     });
+=======
+  ws.on("message", function message(data: any) {
+    const message = JSON.parse(data);
+    console.log(message);
+  });
+>>>>>>> e553265 (Created http server to verify the status of websocket)
 
-    ws.send('something');
-})
+  ws.send("something");
+});
 
+<<<<<<< HEAD
 server.listen(6969);
+=======
+const PORT = 6969;
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
+>>>>>>> e553265 (Created http server to verify the status of websocket)

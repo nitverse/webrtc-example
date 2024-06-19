f2d0aa28c8aeaf1512d21160a115f624e71526e6
nitverse <nmakwana496@gmail.com>
2024-06-19 22:35:31 +0530

    Created http server to verify the status of websocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,27 +20,15 @@ let recieverSocket: null | WebSocket = null;
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
-<<<<<<< HEAD
-    ws.on('message', function message(data: any) {
-        const message = JSON.parse(data);
-     //   console.log(message);
-        
-    });
-=======
   ws.on("message", function message(data: any) {
     const message = JSON.parse(data);
     console.log(message);
   });
->>>>>>> e553265 (Created http server to verify the status of websocket)
 
   ws.send("something");
 });
 
-<<<<<<< HEAD
-server.listen(6969);
-=======
 const PORT = 6969;
 server.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
->>>>>>> e553265 (Created http server to verify the status of websocket)
